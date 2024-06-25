e8675cc1c97fa85623c7110d59d1ce705fab7c34
ashikur <ashikur1602@gmail.com>
2024-06-25 16:44:27 +0600

    user related api and contact related api created succesfully

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,18 +1,18 @@
 const asyncHandler = require("express-async-handler");
 // @desc Get all contacts
 // @route GET /api/contacts
-// @access public
+// @access private
 
 const Contact = require("../models/contactModel");
 const getContacts = asyncHandler(async (req, res) => {
-    const contacts = await Contact.find();
+    const contacts = await Contact.find({user_id: req.user.id});
     res.status(200).json(contacts);
 });
 
 
 // @desc Create contacts
 // @route POST /api/contacts
-// @access public
+// @access private
 const createContact = asyncHandler(async (req, res) => {
     console.log("The request body is: ", req.body);
     const { name, email, phone } = req.body;
@@ -25,6 +25,7 @@ const createContact = asyncHandler(async (req, res) => {
         name,
         email,
         phone,
+        user_id: req.user.id
     });
 
     res.status(201).json(contact);
@@ -33,7 +34,7 @@ const createContact = asyncHandler(async (req, res) => {
 
 // @desc Get contact
 // @route GET /api/contacts/:id
-// @access public
+// @access private
 const getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
@@ -45,12 +46,17 @@ const getContact = asyncHandler(async (req, res) => {
 
 // @desc Update contact
 // @route PUT /api/contacts/:id
-// @access public
+// @access private
 const updateContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
         res.status(404);
         throw new Error("Contact not found");
+    }
+
+    if (contact.user_id.toString() !== req.user.id) {
+        res.status(403);
+        throw new Error("User don't have permission to updae other user contacts");
     }
 
     const updatedContact = await Contact.findByIdAndUpdate(
@@ -67,14 +73,14 @@ const updateContact = asyncHandler(async (req, res) => {
 
 // @desc Delete contact
 // @route DELETE /api/contacts/:id
-// @access public
+// @access private
 const deleteContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
     if (!contact) {
         res.status(404);
         throw new Error("Contact not found");
     }
-    await Contact.deleteOne();
+    await Contact.deleteOne({_id: req.params.id});
     res.status(200).json(contact)
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDb = require("./config/dbConnection")
 const errorHandler = require("./middleware/errorHandler");
+
 const dotenv = require("dotenv").config();
 
 connectDb();
@@ -11,6 +12,7 @@ app.use(express.json());
 
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
